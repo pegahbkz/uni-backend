f0f7f3355b9e2e93f7f79344b0bb681bd984098a
pegahbkz <p.beikzadeh@gmail.com>
2023-04-27 17:45:20 +0430

    connect-to-mongoose
    connected server to mongoose

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
+require('dotenv').config()
+
 
 //fetch libraries
-const express = require("express");
-//config server
+const express = require("express")
+const mongoose = require('mongoose')
+
 const app = express()
 
+//connect server to mongoose
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 
+//check for error in connecting to database
+const db = mongoose.connection
+db.on('error', (error) => 
+  console.error(error))
+
+//log message when we connect to database for the first time
+db.once('open', () =>
+  console.log('Connected to database'))
+
+app.use(express.json())
 
 //routes
 
 
 //listen at environment valuable port or 3000
-const PORT = process.env.PORT || 3000;
+//kill -9 $(lsof -t -i:3000)
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`)
 });
 
