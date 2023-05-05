b59bd973cd216e74454d6156692d7da3d3a95609
pegahbkz <p.beikzadeh@gmail.com>
2023-05-05 11:46:56 +0330

    update

--- routes/index.js
+++ routes/index.js
@@ -18,4 +18,24 @@ router.get('/Professors', async (req, res) => {
     }
 })
 
+router.post('/Professor', async (req, res) => {
+    const {firstname, lastname, id, password, email, 
+        phonenumber, rank, department, major} = req.body
+
+    const professor = new Professor({
+        firstname, lastname, id, password, email, 
+        phonenumber, rank, department, major
+    }) 
+
+    try {
+        const newProfessor = await professor.save()
+        //201: successfully created object
+        res.status(201).json(newProfessor)
+    }
+    catch (err) {
+        //400: wrong user input
+        res.status(400).json({message : err.message})
+    }
+})
+
 module.exports = router
